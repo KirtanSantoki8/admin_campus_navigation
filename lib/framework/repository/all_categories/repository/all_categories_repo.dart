0875b5a8b8b9a63b3331de4feaaa9a910b414b11
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-20 19:52:13 +0530

    All Categories repository

---
+++ lib/framework/repository/all_categories/repository/all_categories_repo.dart
@@ -0,0 +1,57 @@
+import 'package:admin_campus_navigation/framework/repository/all_categories/contract/all_categories_contract.dart';
+import 'package:admin_campus_navigation/framework/repository/all_categories/model/all_categories_model.dart';
+import 'package:admin_campus_navigation/framework/repository/all_categories/model/all_categories_no_data_model.dart';
+import 'package:dio/dio.dart';
+
+import '../../../providers/network/dio/dio_client.dart';
+import '../../../providers/network/endpoints/api_endpoints.dart';
+
+class AllCategoriesRepo implements AllCategoriesContract{
+
+  DioClient dioClient = DioClient();
+
+  @override
+  Future<AllCategoriesModel> getAllDefaultCategories() async {
+    try{
+      var response = await dioClient.get(ApiEndpoints.allDefaultCategories);
+
+      if(response.statusCode == 200){
+        return AllCategoriesModel.fromJson(response.data);
+      }else{
+        throw Exception('Something went wrong');
+      }
+    }
+    on DioException catch (e){
+      throw Exception(e.message);
+    }
+    catch(e){
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<AllCategoriesModel> getAllCustomCategories() async {
+    try{
+      print('getAllCustomCategories');
+      var response = await dioClient.get(ApiEndpoints.allCustomCategories);
+
+      if(response.statusCode == 200){
+        if(AllCategoriesModel.fromJson(response.data).status == 400){
+          throw Exception(AllCategoriesNoDataModel.fromJson(response.data).message);
+        }else{
+          print(response.data);
+          return AllCategoriesModel.fromJson(response.data);
+        }
+      }else{
+        throw Exception('Something went wrong');
+      }
+    }
+    on DioException catch (e){
+      throw Exception(e.message);
+    }
+    catch(e){
+      throw Exception(e.toString());
+    }
+  }
+
+}
