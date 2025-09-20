1b2ed1dc96a68f5a4574bc41cac623bdfd4b11de
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-20 19:50:46 +0530

    Endpoint added for all categories

--- lib/framework/providers/network/endpoints/api_endpoints.dart
+++ lib/framework/providers/network/endpoints/api_endpoints.dart
@@ -3,4 +3,6 @@ class ApiEndpoints{
   static final baseUrl = 'https://guni-nevigation-api.onrender.com';
 
   static final dashboard = '/dashboard';
+  static final allDefaultCategories = '/getAllMainLocations';
+  static final allCustomCategories = '/getAllLocation';
 }
