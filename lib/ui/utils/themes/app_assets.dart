74f4e1900f29d24947b9ea74d86e4f2b041fdb56
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-20 19:50:17 +0530

    Assets Added

--- lib/ui/utils/themes/app_assets.dart
+++ lib/ui/utils/themes/app_assets.dart
@@ -1,3 +1,12 @@
-class AppAssets{
-
+class AppAssets {
+  static const String canteen = 'assets/images/canteen.png';
+  static const String colleges = 'assets/images/colleges.png';
+  static const String guniLogo = 'assets/images/guni_logo.png';
+  static const String gym = 'assets/images/gym.png';
+  static const String hospital = 'assets/images/hospital.png';
+  static const String hostel = 'assets/images/hostel.png';
+  static const String mess = 'assets/images/mess.png';
+  static const String parking = 'assets/images/parking.png';
+  static const String cart = 'assets/svgs/shopping-cart.svg';
+  static const String sports = 'assets/svgs/sport-ball.svg';
 }
