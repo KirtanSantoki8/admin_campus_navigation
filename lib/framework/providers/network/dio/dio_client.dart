b8742ddb476f03c3fb19cb0df22f62f7ebce2c12
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-20 17:00:03 +0530

    Created dio object and added log interceptor also created common get method

---
+++ lib/framework/providers/network/dio/dio_client.dart
@@ -0,0 +1,15 @@
+import 'package:admin_campus_navigation/framework/providers/network/endpoints/api_endpoints.dart';
+import 'package:dio/dio.dart';
+
+class DioClient{
+
+  final dio = Dio(BaseOptions(baseUrl: ApiEndpoints.baseUrl));
+
+  DioClient(){
+    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+  }
+
+  Future<Response> get(String url) async{
+    return await dio.get(url);
+  }
+}
