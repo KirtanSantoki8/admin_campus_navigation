800f8e3eb7c895e35685647483baff9135d33406
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-20 16:56:57 +0530

    Added provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:admin_campus_navigation/ui/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
