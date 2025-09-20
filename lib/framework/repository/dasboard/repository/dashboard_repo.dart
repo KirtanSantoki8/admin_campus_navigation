e3ec8b9d45dc0411421264a85f4e81ee1c63de6f
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-20 17:02:04 +0530

    Repository for dashboard screen

---
+++ lib/framework/repository/dasboard/repository/dashboard_repo.dart
@@ -0,0 +1,32 @@
+import 'package:admin_campus_navigation/framework/providers/network/dio/dio_client.dart';
+import 'package:admin_campus_navigation/framework/repository/dasboard/model/dashboard_model.dart';
+import 'package:dio/dio.dart';
+
+import '../../../providers/network/endpoints/api_endpoints.dart';
+import '../contract/dashboard_contract.dart';
+
+class DashboardRepo implements DashboardContract{
+
+  DioClient dioClient = DioClient();
+
+  @override
+  Future<DashboardModel> getDashboardData() async {
+    try{
+      var response = await dioClient.get(ApiEndpoints.dashboard);
+
+      if(response.statusCode == 200){
+        print(DashboardModel.fromJson(response.data));
+        return DashboardModel.fromJson(response.data);
+      }else{
+        throw Exception('Something went wrong');
+      }
+    }
+    on DioException catch (e){
+      throw Exception(e.message);
+    }
+    catch(e){
+      throw Exception(e.toString());
+    }
+  }
+
+}
