a1f2c98d6b8e418e1df4f0f85fddaf4ded344d36
KirtanPatel8 <kirtansantoki12@gmail.com>
2025-09-21 13:28:17 +0530

    Solved validation issue

--- lib/ui/screens/add_edit_category_screen.dart
+++ lib/ui/screens/add_edit_category_screen.dart
@@ -18,30 +18,33 @@ class AddEditCategoryScreen extends ConsumerWidget {
     return Center(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          shrinkWrap: true,
-          children: [
-            Center(
-              child: Text(
-                'Preview',
-                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              Center(
+                child: Text(
+                  'Preview',
+                  style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            Card(
-              child: LayoutBuilder(
-                builder: (BuildContext context, BoxConstraints constraints) {
-                  return Form(
-                    key: formKey,
-                    child: Padding(
+              Card(
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Image.file(
-                            File(ref.watch(imagePickerProvider)),
-                            height: 150,
-                            width: constraints.maxWidth * 0.8,
-                            fit: BoxFit.contain,
+                          Visibility(
+                            visible: ref.watch(imagePickerProvider) != '',
+                            child: Image.file(
+                              File(ref.watch(imagePickerProvider)),
+                              height: 150,
+                              width: constraints.maxWidth * 0.8,
+                              fit: BoxFit.contain,
+                            ),
                           ),
                           SizedBox(height: 20),
                           ValueListenableBuilder(
@@ -54,91 +57,91 @@ class AddEditCategoryScreen extends ConsumerWidget {
                           ),
                         ],
                       ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.center,
-              child: CommonButton(
-                onPressed: () async {
-                  var image = await ImagePicker().pickImage(
-                    source: ImageSource.gallery,
-                  );
-                  if (image != null) {
-                    ref
-                        .watch(imagePickerProvider.notifier)
-                        .pickImage(image.path);
-                  }
-                },
-                child: Text('Pick Image'),
-              ),
-            ),
-            SizedBox(height: 20),
-            CommonTextField(
-              hintText: 'Category Name',
-              labelText: 'Category Name',
-              controller: ref
-                  .read(addCategoryProvider.notifier)
-                  .categoryNameController,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter category name';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.center,
-              child: CommonButton(
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    if (ref.watch(imagePickerProvider) == '') {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Please select image')),
-                      );
-                      return;
-                    } else {
-                      ref
-                          .read(addCategoryProvider.notifier)
-                          .addCategory()
-                          .whenComplete(
-                            () => ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Category added successfully'),
-                              ),
-                            ),
-                          );
-                    }
-                  }
-                },
-                child: Text('Add Category'),
-              ),
-            ),
-            SizedBox(height: 20,),
-            ref
-                .watch(addCategoryProvider)
-                .when(
-                  data: (data) {
-                    return Visibility(
-                      visible: false,
-                      child: Text(data.message ?? ''),
                     );
-                  },
-                  error: (error, stackTrace) {
-                    return Visibility(
-                      visible: false,
-                      child: Text(error.toString()),
-                    );
-                  },
-                  loading: () {
-                    return Center(child: CircularProgressIndicator());
                   },
                 ),
-          ],
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.center,
+                child: CommonButton(
+                  onPressed: () async {
+                    var image = await ImagePicker().pickImage(
+                      source: ImageSource.gallery,
+                    );
+                    if (image != null) {
+                      ref
+                          .watch(imagePickerProvider.notifier)
+                          .pickImage(image.path);
+                    }
+                  },
+                  child: Text('Pick Image'),
+                ),
+              ),
+              SizedBox(height: 20),
+              CommonTextField(
+                hintText: 'Category Name',
+                labelText: 'Category Name',
+                controller: ref
+                    .read(addCategoryProvider.notifier)
+                    .categoryNameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter category name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.center,
+                child: CommonButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      if (ref.watch(imagePickerProvider) == '') {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Please select image')),
+                        );
+                        return;
+                      } else {
+                        ref
+                            .read(addCategoryProvider.notifier)
+                            .addCategory()
+                            .whenComplete(
+                              () => ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Category added successfully'),
+                                ),
+                              ),
+                            );
+                      }
+                    }
+                  },
+                  child: Text('Add Category'),
+                ),
+              ),
+              SizedBox(height: 20,),
+              ref
+                  .watch(addCategoryProvider)
+                  .when(
+                    data: (data) {
+                      return Visibility(
+                        visible: false,
+                        child: Text(data.message ?? ''),
+                      );
+                    },
+                    error: (error, stackTrace) {
+                      return Visibility(
+                        visible: false,
+                        child: Text(error.toString()),
+                      );
+                    },
+                    loading: () {
+                      return Center(child: CircularProgressIndicator());
+                    },
+                  ),
+            ],
+          ),
         ),
       ),
     );
